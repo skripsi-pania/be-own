532a4b4ded13e1168e87447ccec0c04d3b26dbd9
vania <sk080501@gmail.com>
2023-05-21 20:05:59 +0700

    update text welcomee

--- server.js
+++ server.js
@@ -1,62 +1,50 @@
-const morgan = require('morgan');
-const multer = require('multer');
-const path = require('path');
+const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 
 const dbConfig = require("./config/db.config");
 
-
-
-
-
 const app = express();
 
 const corsOptions = {
-
-  origin: "*"
+  origin: "*",
 };
-
-
 
 app.use(cors(corsOptions));
 
-
 // parse requests of content-type - application/json
 app.use(express.json());
-if(process.env.MODE === 'development'){
-  app.use(morgan('dev'))
+if (process.env.MODE === "development") {
+  app.use(morgan("dev"));
 }
 // parse requests of content-type - application/x-www-form-urlencoded
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const db = require("./models");
 const Role = db.role;
 
-dbConfig.
-  then(() => {
+dbConfig
+  .then(() => {
     console.log("Successfully connect to MongoDB.");
     initial();
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Connection error", err);
     process.exit();
   });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to voluntegreen application." });
+  res.json({ message: "BISMILLAH SKRIPSI VANIAA" });
 });
 
-
-const allRouter = require('./routes/')
-app.use(allRouter)
-
+const allRouter = require("./routes/");
+app.use(allRouter);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -68,8 +56,8 @@ function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
-        name: "user"
-      }).save(err => {
+        name: "user",
+      }).save((err) => {
         if (err) {
           console.log("error", err);
         }
@@ -78,8 +66,8 @@ function initial() {
       });
 
       new Role({
-        name: "admin"
-      }).save(err => {
+        name: "admin",
+      }).save((err) => {
         if (err) {
           console.log("error", err);
         }
@@ -87,6 +75,5 @@ function initial() {
         console.log("added 'admin' to roles collection");
       });
     }
-  })
+  });
 }
-
